e6711b579fada28ef4865753eea85e528d9661b5
wox <69124342+muratgzl@users.noreply.github.com>
2022-11-29 00:29:16 +0300

    Add files via upload

---
+++ generated-pass.py
@@ -0,0 +1,14 @@
+
+
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+digits = "0123456789"
+symbols = "@#$%&/\?"
+
+use_for = lower_case + upper_case + digits + symbols
+length_for_pass = 10
+password = "".join(random.sample(use_for, length_for_pass))
+
+print("Your generated password: " + password)
